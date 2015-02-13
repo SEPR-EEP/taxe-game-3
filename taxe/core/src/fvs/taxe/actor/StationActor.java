1f6684b5e5fa01fcf65e47a84467f01e4b5317a7
chrisbucklenufc <chrisbucklenufc@gmail.com>
2015-02-13 11:02:06 +0000

    Station moving bugfix
    Stations now longer go for a wander when goals are clicked repeatedly

--- taxe/core/src/fvs/taxe/actor/StationActor.java
+++ taxe/core/src/fvs/taxe/actor/StationActor.java
@@ -11,30 +11,35 @@ import com.badlogic.gdx.utils.Timer.Task;
 import gameLogic.map.IPositionable;
 
 public class StationActor extends Image {
-    private static final int STATION_SIZE_INCREASE = 10;	// the size the station increases (x + y) when the station selected()
+	private static final int STATION_SIZE_INCREASE = 10;	// the size the station increases (x + y) when the station selected()
 	private static final float SELECTION_DURATION = 3f;		// the amount of time the selection stays active
-	private static int width = 20;
-    private static int height = 20;
+	private int width = 20;
+	private int height = 20;
+	private boolean selected = false;						// prevents multiple clicks moving the station
 
-    public StationActor(IPositionable location) {
-        super(new Texture(Gdx.files.internal("station_dot.png")));
-        
-        setSize(width, height);
-        setPosition(location.getX() - width / 2, location.getY() - height / 2);
-    }
+	public StationActor(IPositionable location) {
+		super(new Texture(Gdx.files.internal("station_dot.png")));
+
+		setSize(width, height);
+		setPosition(location.getX() - width / 2, location.getY() - height / 2);
+	}
 
 	public void selected() {
-		setPosition(getX()-STATION_SIZE_INCREASE/2,getY()-STATION_SIZE_INCREASE/2);
-		setSize(width+STATION_SIZE_INCREASE, height +STATION_SIZE_INCREASE);
-		
-		Timer.schedule(new Task(){
-		    @Override
-		    public void run() {
-		    	setPosition(getX()+STATION_SIZE_INCREASE/2,getY()+STATION_SIZE_INCREASE/2);
-		    	setSize(width, height);
-		    }
-		}, SELECTION_DURATION);
+		if (!selected){
+			selected = true;
+			setPosition(getX()-STATION_SIZE_INCREASE/2,getY()-STATION_SIZE_INCREASE/2);
+			setSize(width+STATION_SIZE_INCREASE, height +STATION_SIZE_INCREASE);
+
+			Timer.schedule(new Task(){
+				@Override
+				public void run() {
+					setPosition(getX()+STATION_SIZE_INCREASE/2,getY()+STATION_SIZE_INCREASE/2);
+					setSize(width, height);
+					selected =false;
+				}
+			}, SELECTION_DURATION);
+		} 
 	}
-    
-    
+
+
 }
